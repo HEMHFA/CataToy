386228843c1c7f3890989e834eafb47da90e52d6
HEMHFA <arnaud.hugo@orange.fr>
2017-09-19 11:47:51 +0200

    Affectation d'une catégorie à un jouet

--- categorie.js
+++ categorie.js
@@ -1,12 +1,24 @@
 class Categorie {
 
     constructor(unLibelle) {
-        this.libelle = unLibelle;
+        this._libelle = unLibelle;
+        this._lesJouets = [];
     }
 
     get libelle(){
         return this._libelle;
     }
+
+    ajouterJouets(unJouet){
+        this._lesJouets.push(unJouet);
+    }
+
+    afficherJouets(display){
+        this._lesJouets.forEach(function (unJouet){
+            display(unJouet.libelle);
+        });
+    }
+
 
 }
 module.exports = Categorie;
