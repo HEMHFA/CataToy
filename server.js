34742ee5a78a3b68bc4ccf235273e78055163fec
HEMHFA <arnaud.hugo@orange.fr>
2017-09-19 11:07:52 +0200

    Creation de la classe TrancheAge

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const Jouet = require('./jouet.js');
+const TranchAge = require('./trancheAge.js');
+
+console.log("Bienvenue dans le catalogue de jouet");
+
+let unJouet = new Jouet("Draisienne junior bleue");
+console.log(unJouet.libelle);
+
+let tranche25 = new TrancheAge("tranche25",2,5);
+console.log(tranche25.toString());
